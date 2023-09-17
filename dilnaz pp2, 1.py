f231b742c14e7d98ab92755c824484541091b256
dilnazzhusipbekova <144439677+dilnazzhusipbekova@users.noreply.github.com>
2023-09-17 22:49:19 +0600

    Add files via upload

---
+++ dilnaz pp2, 1.py
@@ -0,0 +1,52 @@
+#1st task
+a = int(input())
+b = int(input())
+c = int(input())
+print(a + b + c)
+
+#2nd task
+b = int(input())
+h = int(input())
+print(0.5 * b * h)
+
+#3rd task
+# Число n можно считать так:
+n = int(input())
+k = int(input())
+
+# Выводите результат через print()
+
+# Пример на деление, вычисление частного и остатка:
+print(k // n)
+print(k %n)
+
+
+#4th task
+n=int(input())
+hours = n % (60 * 24) // 60
+min = n % 60
+print(hours, min)
+
+#5th task
+name = input()
+print('Hello, ' + name + '!')
+
+#6th task
+n = int(input())
+k = n - 1
+p = n + 1
+print("The next number for the number " + str(n) + " is "
++  str(p) )
+print("The previous number for the number " + str(n) + " is " + str(k))
+
+#7th task
+a = int(input())
+b = int(input())
+c = int(input())
+print(a//2+b//2+c//2+a%2+b%2+c%2)
+
+#8th task
+a = int(input())
+b = int(input())
+c = int(input())
+print(a//2+b//2+c//2+a%2+b%2+c%2)
