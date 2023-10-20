2eed02003e209f7b3a5bfacbeb6ec0aee13c1219
dilnazzhusipbekova <144439677+dilnazzhusipbekova@users.noreply.github.com>
2023-10-20 15:43:39 +0600

    lab #05

---
+++ lab5/classes/classes, 1st task.py
@@ -0,0 +1,7 @@
+class StringManipulator:
+    def __init__(self):
+        self.input_string = ""
+    def getString(self):
+        self.input_string = input("")
+    def printString(self):
+        print(self.input_string.upper())
